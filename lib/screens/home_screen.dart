f4b91514e7285dc6d2c639009a304d3187e827c2
codejay2018 <codejay2018@gmail.com>
2025-08-25 13:40:03 +0900

    6 1 AppBar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 5,
+        title: Text('Today`s Toons', style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
