a866cda07f776d8d2f7547be2e53daf06a8c0e1a
codejay2018 <codejay2018@gmail.com>
2025-08-26 13:19:41 +0900

    6 8 Webtoon Card - 카드부분을 함수로 뺌 - Image.network()으로 이미지 출력 - BoxShadow()로 그림자 표현

--- lib/screens/home_screen_stateless.dart
+++ lib/screens/home_screen_stateless.dart
@@ -22,21 +22,54 @@ class HomeScreenStateless extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                print(index);
-                var title = snapshot.data![index].title;
-                return Text(' $index. $title');
-              },
-              separatorBuilder: (context, index) => SizedBox(width: 20),
+            return Column(
+              children: [
+                SizedBox(height: 100),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
-
           return Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var toon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withAlpha(100),
+                  ),
+                ],
+              ),
+              width: 250,
+              child: Image.network(
+                toon.thumb,
+                headers: {
+                  "Referer": "https://comic.naver.com", // 네이버 서버가 요구하는 값
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(toon.title, style: TextStyle(fontSize: 22)),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 20),
     );
   }
 }
